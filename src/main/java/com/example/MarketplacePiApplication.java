f8e3555191970f70badf13b601311c36584fcee4
Nahed <nahedharrath@gmail.com>
2025-04-08 22:48:25 +0100

    Résolution des conflits lors du merge de user

--- src/main/java/com/example/MarketplacePiApplication.java
+++ src/main/java/com/example/MarketplacePiApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.EduNext", "com.example.marketplacepi"})
+
 public class MarketplacePiApplication {
 
 	public static void main(String[] args) {
